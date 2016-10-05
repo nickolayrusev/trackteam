63b263c365b77e4b757117870d1999692e8db8f9
nickolay rusev <nickolay.rusev@gmail.com>
2016-10-05 19:02:52 +0300

    in the middle of migration from other db

---
+++ migrator/src/main/java/com/nrusev/MigratorApplication.java
@@ -0,0 +1,12 @@
+package com.nrusev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratorApplication.class, args);
+	}
+}
---
+++ migrator/src/test/java/com/nrusev/MigratorApplicationTests.java
@@ -0,0 +1,28 @@
+package com.nrusev;
+
+import com.nrusev.repository.MatchesRepository;
+import com.nrusev.repository.TeamRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MigratorApplication.class)
+public class MigratorApplicationTests {
+
+	@Autowired
+	MatchesRepository matchesRepository;
+
+
+	@Autowired
+	TeamRepository teamRepository;
+
+	@Test
+	public void contextLoads() {
+//		matchesRepository.findAllMatches();
+		System.out.println(teamRepository.findAllClubTeams());
+	}
+
+}
