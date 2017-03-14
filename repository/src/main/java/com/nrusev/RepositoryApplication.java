678cf76a8ae1a8c883d21cb204a84e2b4a69dea9
nickolay rusev <nickolay.rusev@gmail.com>
2017-03-14 15:32:37 +0200

    heroku test

--- repository/src/main/java/com/nrusev/RepositoryApplication.java
+++ repository/src/main/java/com/nrusev/RepositoryApplication.java
@@ -1,10 +1,6 @@
 package com.nrusev;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
