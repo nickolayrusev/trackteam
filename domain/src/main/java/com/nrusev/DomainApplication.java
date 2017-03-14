e52292b325d2970fa87d9a3d1ffbe2078fbcf3b8
nickolay rusev <nickolay.rusev@gmail.com>
2017-03-14 14:45:26 +0200

    fixing build in heroku

---
+++ domain/src/main/java/com/nrusev/DomainApplication.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Nikolay Rusev on 14.3.2017 г..
+ */
+
+package com.nrusev;
+
+import org.springframework.boot.SpringApplication;
+
+public class DomainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DomainApplication.class, args);
+    }
+}
